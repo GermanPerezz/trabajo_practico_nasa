22109c812aebdd56313b65432b34774015a8a0d9
GerSchroeder37 <gerimport37@gmail.com>
2024-06-18 23:40:08 -0300

    Galería principal de imágenes

--- nasa_image_gallery/views.py
+++ nasa_image_gallery/views.py
@@ -12,7 +12,7 @@ def index_page(request):
 
 # auxiliar: retorna 2 listados -> uno de las imágenes de la API y otro de los favoritos del usuario.
 def getAllImagesAndFavouriteList(request):
-    images = []
+    images = services_nasa_image_gallery.getAllImages()
     favourite_list = []
 
     return images, favourite_list
@@ -21,8 +21,8 @@ def getAllImagesAndFavouriteList(request):
 def home(request):
     # llama a la función auxiliar getAllImagesAndFavouriteList() y obtiene 2 listados: uno de las imágenes de la API y otro de favoritos por usuario*.
     # (*) este último, solo si se desarrolló el opcional de favoritos; caso contrario, será un listado vacío [].
-    images = []
-    favourite_list = []
+    images,favourite_list = getAllImagesAndFavouriteList(request)
+    
     return render(request, 'home.html', {'images': images, 'favourite_list': favourite_list} )
 
 
